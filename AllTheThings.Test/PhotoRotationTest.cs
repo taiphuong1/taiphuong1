e2d47c665716c47d956904ea1127c77cf8bb89e2
Tai Phuong <147455400+taiphuong1@users.noreply.github.com>
2024-11-14 10:44:28 -0500

    update photorotationtest handle all 8 orientation

--- AllTheThings.Test/PhotoRotationTest.cs
+++ AllTheThings.Test/PhotoRotationTest.cs
@@ -8,14 +8,15 @@ namespace AllTheThings.Test
 	{
 		public static void GetImageMetadataTest()
 		{
-			string inputImagePath = @"./Images/portrait.jpg";//@"C:\tmp\IDPaaS\debug\portrait.jpg";
-			string outputImagePath = "C:\\tmp\\IDPaaS\\debug\\portrait_rotate.jpg";
+			string inputImagePath = @"./Images/2.jpg";//@"C:\tmp\IDPaaS\photo\iphone\IMG_6059.jpeg";
+			string outputImagePath = "./Images/2_updated.jpg";// "C:\\tmp\\IDPaaS\\photo\\iphone\\updated.jpg";
 
 			// Load image
 			using (Image inputImage = Image.Load(inputImagePath))
 			{
 				var exifProfile = inputImage.Metadata?.ExifProfile;
 				var values = exifProfile?.Values;
+				bool isImageUpdated = false;
 				if (values != null)
 				{
 					PrintExifValues(values);
@@ -23,16 +24,52 @@ namespace AllTheThings.Test
 					//exifProfile!.TryGetValue(ExifTag.Orientation, out var orientationValue);
 					//exifProfile!.TryGetValue(ExifTag.PixelXDimension, out var widthValue);
 					//exifProfile!.TryGetValue(ExifTag.PixelYDimension, out var heightValue);
-
 					//if (widthValue!.Value > heightValue!.Value && orientationValue!.Value == 6)
+
 					if (exifProfile!.TryGetValue(ExifTag.Orientation, out var orientationValue))
 					{
-						if (orientationValue.Value == 6)
+						switch (orientationValue.Value)
 						{
-							inputImage.Mutate(x => x.Rotate(90));
-							inputImage.Metadata!.ExifProfile!.SetValue(ExifTag.Orientation, (ushort)1);
+							case 1: //Horizontal (normal)
+								break;
+							case 2: //Mirror horizontal
+								inputImage.Mutate(x => x.Flip(FlipMode.Horizontal));
+								isImageUpdated = true;
+								break;
+							case 3: //Rotate 180 CW
+								inputImage.Mutate(x => x.Rotate(180));
+								isImageUpdated = true;
+								break;
+							case 4: //Mirror vertical
+								inputImage.Mutate(x => x.Flip(FlipMode.Vertical));
+								isImageUpdated = true;
+								break;
+							case 5: //Mirror horizontal and rotate 270 CW
+								inputImage.Mutate(x => x.RotateFlip(RotateMode.Rotate270, FlipMode.Vertical)); //Rotate first then Flip
+								isImageUpdated = true;
+								break;
+							case 6: //Rotate 90 CW
+								inputImage.Mutate(x => x.Rotate(90));
+								isImageUpdated = true;
+								break;
+							case 7: //Mirror horizontal and rotate 90 CW
+								inputImage.Mutate(x => x.RotateFlip(RotateMode.Rotate90, FlipMode.Vertical)); //Rotate first then Flip
+								isImageUpdated = true;
+								break;
+							case 8: //Rotate 270 CW
+								inputImage.Mutate(x => x.Rotate(270));
+								isImageUpdated = true;
+								break;
+							default:
+								break;
+						}
+
+						if (isImageUpdated)
+						{
+							inputImage.Metadata!.ExifProfile!.SetValue(ExifTag.Orientation, (ushort)1); //set it back to normal
 							inputImage.Save(outputImagePath);
 
+							//Re-print the Exif metadata
 							Image outputImage = Image.Load(outputImagePath);
 							var oExifProfile = outputImage.Metadata?.ExifProfile;
 							var oValues = oExifProfile?.Values;
